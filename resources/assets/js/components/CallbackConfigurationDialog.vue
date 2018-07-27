a240b834d554ec4fe4094f13c1f0a0d31237317b
pitsi <szana8@gmail.com>
2018-07-27 20:42:27 +0200

    Fix the duplication detection issue and change component location

--- resources/assets/js/components/CallbackConfigurationDialog.vue
+++ resources/assets/js/components/CallbackConfigurationDialog.vue
@@ -1,77 +1,47 @@
 <template>
     <div>
-        <div class="modal fade"
-             id="callback-configuration"
-             tabindex="-1"
-             role="dialog"
-             aria-hidden="true">
-            <div class="modal-dialog modal-lg"
-                 role="document">
+        <div class="modal fade" id="callback-configuration" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">
                             Callback
-                            Configuration</h5>
-                        <button type="button"
-                                class="close"
-                                data-dismiss="modal"
-                                aria-label="Close">
+                            Configuration
+                        </h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <ul class="nav nav-tabs"
-                            id="myTab"
-                            role="tablist">
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active"
-                                   id="home-tab"
-                                   data-toggle="tab"
-                                   href="#home"
-                                   role="tab"
-                                   aria-controls="home"
-                                   aria-selected="true">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
                                     Validators
                                     <span class="badge badge-secondary">{{ this.rules.length }}</span>
                                 </a>
+                            <li class="nav-item">
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
+                                    Pre Functions
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link"
-                                   id="profile-tab"
-                                   data-toggle="tab"
-                                   href="#profile"
-                                   role="tab"
-                                   aria-controls="profile"
-                                   aria-selected="false">Pre Functions</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link"
-                                   id="contact-tab"
-                                   data-toggle="tab"
-                                   href="#contact"
-                                   role="tab"
-                                   aria-controls="contact"
-                                   aria-selected="false">Post Functions</a>
+                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">
+                                    Post Functions
+                                </a>
                             </li>
                         </ul>
-                        <div class="tab-content"
-                             id="myTabContent">
+                        <div class="tab-content" id="myTabContent">
                             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-
                                 <div class="form-row mt-3">
                                     <div class="form-group col-md-5">
                                         <label>Select validator</label>
                                         <select class="form-control" v-if="callbackObjects" v-model="rule">
-                                            <option :value="validator.validator"
-                                                    :key="validator.name"
-                                                    v-for="validator in callbackObjects.validators"
-                                                    v-text="validator.name">
-                                            </option>
+                                            <option :value="validator.validator" :key="validator.name" v-for="validator in callbackObjects.validators" v-text="validator.name"></option>
                                         </select>
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label>Select field</label>
-                                        <input type="text" class="form-control" v-model="field" />
+                                        <input type="text" class="form-control" v-model="field"/>
                                     </div>
                                     <div class="form-group col-md-1">
                                         <label>Action</label>
@@ -86,7 +56,7 @@
                                             <button type="button" class="close" data-dismiss="alert" aria-label="Close" @click="removeRule(rule)">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
-                                            <br />labels
+                                            <br/>labels
                                             <small>{{ this.callbackObjects.validators[rule[Object.keys(rule)[0]]].description }}</small>
                                         </div>
                                     </div>
@@ -121,7 +91,7 @@
 </template>
 
 <script>
-    import { EventBus } from "../event-bus.js";
+    import {EventBus} from "../event-bus.js";
 
     export default {
         data() {
@@ -139,8 +109,6 @@
         mounted() {
             // Initialize event handler to show the modal if the event has been fired
             EventBus.$on("showConfigureCallbacksModal", this.show);
-
-            //this.rules = this.validators;
         },
 
         methods: {
@@ -167,11 +135,13 @@
                 this.field = null;
             },
 
+            // Remove the selected rule form the rule set object
             removeRule(rule) {
                 let key = Object.keys(rule)[0];
-                this.rules = this.rules.filter( el => el[key] !== rule[key] );
+                this.rules = this.rules.filter(el => el[key] !== rule[key]);
             },
 
+            // Add the rule set to the appropriate transition
             save() {
                 window.laraflowGo.model.setDataProperty(
                     this.diagram.subject.part.data,
