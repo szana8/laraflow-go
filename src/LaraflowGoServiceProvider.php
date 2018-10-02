ee706102df7619a8b5e548d337dd79af7648229f
Istvan Szana <szana8@gmail.com>
2018-10-02 07:38:51 +0000

    Apply fixes from StyleCI

--- src/LaraflowGoServiceProvider.php
+++ src/LaraflowGoServiceProvider.php
@@ -15,10 +15,10 @@ class LaraflowGoServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/assets/js/components' => base_path('resources/assets/js/components/laraflow/laraflow-go/components'),
+                __DIR__.'/../resources/assets/js/components'    => base_path('resources/assets/js/components/laraflow/laraflow-go/components'),
                 __DIR__.'/../resources/assets/js/LaraflowGo.js' => base_path('resources/assets/js/components/laraflow/laraflow-go/LaraflowGo.js'),
-                __DIR__.'/../resources/assets/js/event-bus.js' => base_path('resources/assets/js/components/laraflow/laraflow-go/event-bus.js'),
-                __DIR__.'/../config/laraflow-go.php' => config_path('laraflow-go.php')
+                __DIR__.'/../resources/assets/js/event-bus.js'  => base_path('resources/assets/js/components/laraflow/laraflow-go/event-bus.js'),
+                __DIR__.'/../config/laraflow-go.php'            => config_path('laraflow-go.php'),
             ], 'laraflow-go-components');
         }
     }
